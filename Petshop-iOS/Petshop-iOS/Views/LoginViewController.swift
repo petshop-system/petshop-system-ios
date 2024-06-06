7a51514a72045ba2590e031194a1f001f592c168
Matheus Ferreira <Mathferreiranasc12@gmail.com>
2024-06-05 22:27:21 -0300

    refactor: modularize UI setup and constraints in LoginViewController

--- Petshop-iOS/Petshop-iOS/Views/LoginViewController.swift
+++ Petshop-iOS/Petshop-iOS/Views/LoginViewController.swift
@@ -9,105 +9,76 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
-    let logo: UIImageView = {
-        let logo = UIImageView()
-        logo.image = UIImage(named: "logo-transparent")
-        logo.translatesAutoresizingMaskIntoConstraints = false
-        logo.contentMode = .scaleAspectFit
-        return logo
-    }()
-    
-    let usernameTextField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "Email"
-        textField.borderStyle = .roundedRect
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        return textField
-    }()
-    
-    let passwordTextField: UITextField = {
-        let textField = UITextField()
-        textField.placeholder = "Senha"
-        textField.borderStyle = .roundedRect
-        textField.isSecureTextEntry = true
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        return textField
-    }()
-    
-    let loginButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.setTitle("Entrar", for: .normal)
-        button.titleLabel?.font = UIFont.systemFont(ofSize: 25)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitleColor(UIColor.white, for: .normal)
-        button.layer.cornerRadius = 15
-        button.backgroundColor = UIColor(hex: "#42A2D9")
-        button.addTarget(LoginViewController.self, action: #selector(login), for: .touchUpInside)
-        return button
-    }()
-    
-    @objc let forgotPasswordLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Esqueceu a senha?"
-        label.textColor = UIColor.systemBlue
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    let companyLabel: UILabel = {
-        let label = UILabel()
-        label.text = "PETSHOP SYSTEM 2024 ®"
-        label.textColor = UIColor.systemBlue
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    let newUserLabel: UILabel = {
-        let label = UILabel()
-        label.text = "Não tem uma conta?"
-        label.textColor = UIColor.black
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    let newUserButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Clique aqui", for: .normal)
-        button.setTitleColor(UIColor.systemBlue, for: .normal)
-        button.addTarget(LoginViewController.self, action: #selector(createAccount), for: .touchUpInside)
-        return button
-    }()
-    
+    let logo = UIImageView()
+    let usernameTextField = UITextField()
+    let passwordTextField = UITextField()
+    let loginButton = UIButton(type: .system)
+    let forgotPasswordLabel = UILabel()
+    let companyLabel = UILabel()
+    let newUserLabel = UILabel()
+    let newUserButton = UIButton()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor(hex: "#E2E8F0")
         
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(forgotPasswordTapped))
-        forgotPasswordLabel.isUserInteractionEnabled = true
-        forgotPasswordLabel.addGestureRecognizer(tapGesture)
+        setupUI()
+        setupConstraints()
+        setupActions()
+    }
+    
+    func setupUI() {
+        logo.image = UIImage(named: "logo-transparent")
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        logo.contentMode = .scaleAspectFit
         
+        usernameTextField.placeholder = "Email"
+        usernameTextField.borderStyle = .roundedRect
+        usernameTextField.translatesAutoresizingMaskIntoConstraints = false
         let emailIcon = UIImageView(image: UIImage(named: "email-icon"))
         emailIcon.contentMode = .center
-
         let rightPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: emailIcon.bounds.height))
-
-        usernameTextField.rightView = rightPaddingView
-        usernameTextField.rightViewMode = .always
         rightPaddingView.addSubview(emailIcon)
         emailIcon.frame = rightPaddingView.bounds
+        usernameTextField.rightView = rightPaddingView
+        usernameTextField.rightViewMode = .always
         
+        passwordTextField.placeholder = "Senha"
+        passwordTextField.borderStyle = .roundedRect
+        passwordTextField.isSecureTextEntry = true
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
         let passwordIcon = UIImageView(image: UIImage(named: "password-icon"))
         passwordIcon.contentMode = .center
-
         let passwordRightPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 40, height: passwordIcon.bounds.height))
-
-        passwordTextField.rightView = passwordRightPaddingView
-        passwordTextField.rightViewMode = .always
         passwordRightPaddingView.addSubview(passwordIcon)
         passwordIcon.frame = passwordRightPaddingView.bounds
-
+        passwordTextField.rightView = passwordRightPaddingView
+        passwordTextField.rightViewMode = .always
+        
+        loginButton.setTitle("Entrar", for: .normal)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+        loginButton.setTitleColor(.white, for: .normal)
+        loginButton.layer.cornerRadius = 15
+        loginButton.backgroundColor = UIColor(hex: "#42A2D9")
+        loginButton.titleLabel?.font = UIFont.systemFont(ofSize: 20)
+        
+        forgotPasswordLabel.text = "Esqueceu a senha?"
+        forgotPasswordLabel.textColor = .systemBlue
+        forgotPasswordLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        companyLabel.text = "PETSHOP SYSTEM 2024 ®"
+        companyLabel.textColor = .systemBlue
+        companyLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        newUserLabel.text = "Não tem uma conta?"
+        newUserLabel.textColor = .black
+        newUserLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        newUserButton.setTitle("Clique aqui", for: .normal)
+        newUserButton.setTitleColor(.systemBlue, for: .normal)
+        newUserButton.translatesAutoresizingMaskIntoConstraints = false
+        
         view.addSubview(logo)
         view.addSubview(usernameTextField)
         view.addSubview(passwordTextField)
@@ -116,13 +87,9 @@ class LoginViewController: UIViewController {
         view.addSubview(newUserLabel)
         view.addSubview(newUserButton)
         view.addSubview(companyLabel)
-        
-        setupConstraints()
     }
     
     func setupConstraints() {
-        logo.translatesAutoresizingMaskIntoConstraints = false
-
         NSLayoutConstraint.activate([
             logo.widthAnchor.constraint(equalToConstant: 250),
             logo.heightAnchor.constraint(equalToConstant: 270),
@@ -157,11 +124,20 @@ class LoginViewController: UIViewController {
             companyLabel.topAnchor.constraint(equalTo: passwordTextField.topAnchor, constant: 350),
         ])
     }
-
+    
+    func setupActions() {
+        loginButton.addTarget(self, action: #selector(login), for: .touchUpInside)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(forgotPasswordTapped))
+        forgotPasswordLabel.isUserInteractionEnabled = true
+        forgotPasswordLabel.addGestureRecognizer(tapGesture)
+        
+        newUserButton.addTarget(self, action: #selector(createAccount), for: .touchUpInside)
+    }
+    
     @objc func login() {
         print("aqui o user vai fazer login no app")
     }
-    
     
     @objc func forgotPasswordTapped() {
         print("aqui o user vai para a view de recuperacao de conta")
