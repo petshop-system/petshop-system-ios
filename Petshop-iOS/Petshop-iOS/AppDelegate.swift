4c86e7eee609828b4382d6d6f682044968f44816
Matheus Ferreira da Silva Nascimento <mathferreiranasc12@gmail.com>
2024-07-13 20:58:57 -0300

    refactor: remove unnecessary line of code

--- Petshop-iOS/Petshop-iOS/AppDelegate.swift
+++ Petshop-iOS/Petshop-iOS/AppDelegate.swift
@@ -13,14 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
-        window = UIWindow(frame: UIScreen.main.bounds)
-        
-        let loginVC = LoginViewController()
-        let navigationController = UINavigationController(rootViewController: loginVC)
-        
-        window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
         
         return true
     }
